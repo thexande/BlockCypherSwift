06c3f304697b74c2d2df1b83cc0594bd4223a485
Alexander Murphy <amurphy345@gmail.com>
2018-05-18 21:45:40 -0600

    adding sources into correct folder

---
+++ BlockCypherSwift/Classes/Wallet.swift
@@ -0,0 +1,12 @@
+struct Wallet: Codable {
+    let address: String
+    let total_received: Int
+    let total_sent: Int
+    let balance: Int
+    let unconfirmed_balance: Int
+    let final_balance: Int
+    let n_tx: Int
+    let unconfirmed_n_tx: Int
+    let final_n_tx: Int
+    let txs: [Transaction]
+}
