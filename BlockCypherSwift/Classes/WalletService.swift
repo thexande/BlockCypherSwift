2fe35d2893edcd2684719237b9344efa425f6538
Alexander Murphy <amurphy345@gmail.com>
2018-05-18 21:50:00 -0600

    adding dateformatter extension and wallet type

--- BlockCypherSwift/Classes/WalletService.swift
+++ BlockCypherSwift/Classes/WalletService.swift
@@ -1,1 +1,54 @@
 import Foundation
+import Result
+
+enum WalletType: String {
+    case bitcoin
+    case litecoin
+    case dogecoin
+    case dash
+    
+    public var symbol: String {
+        switch self {
+        case .bitcoin: return "BTC"
+        case .litecoin: return "LTC"
+        case .dogecoin: return "DOGE"
+        case .dash: return "DASH"
+        }
+    }
+}
+
+struct URLFactory {
+    static func url(walletAddress: String, walletType: WalletType) -> URL? {
+        let address = "https://api.blockcypher.com/v1/\(walletType.symbol.lowercased())/main/addrs/\(walletAddress)/full?limit=50"
+        return URL(string: address)
+    }
+    
+    static let globalCryptoATMEndpoint: URL? = URL(string: "https://www.coinatmfinder.com/CoimATMs-API.php")
+}
+
+enum WalletServiceError: Error {
+    case walletDoesNotExist
+    case urlGenerationFailure
+}
+
+final class WalletService {
+    static func fetchWallet(walletAddress: String, walletType: WalletType, _ completion: @escaping(Result<Wallet, WalletServiceError>) -> Void) {
+        guard let url = URLFactory.url(walletAddress: walletAddress, walletType: walletType) else {
+            completion(.failure(.urlGenerationFailure))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            do {
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .formatted(DateFormatter.iso8601Full)
+                let wallet = try decoder.decode(Wallet.self, from: data)
+                completion(.success(wallet))
+            } catch let error {
+                completion(.failure(.walletDoesNotExist))
+                print(error.localizedDescription)
+            }
+            }.resume()
+    }
+}
