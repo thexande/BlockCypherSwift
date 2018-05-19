7493237c6326f2c60a9366eef1b52a4cf0145d90
Alexander Murphy <amurphy345@gmail.com>
2018-05-18 22:41:22 -0600

    using wallet coordinator

--- Example/BlockCypherSwift/AppDelegate.swift
+++ Example/BlockCypherSwift/AppDelegate.swift
@@ -12,12 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    private let walletCoordinator = WalletCoordinator()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: RootViewController())
+        window?.rootViewController = walletCoordinator.rootViewController
         window?.makeKeyAndVisible()
         
         return true
